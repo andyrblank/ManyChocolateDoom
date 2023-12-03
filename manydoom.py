7db19bf6860c43e844e17b3f0f59e2cff5ab5cb7
Andrew Blank <andy.r.blank@gmail.com>
2023-12-03 13:20:40 -0500

    Changed script to open, resize and move window each time
    This change removes the loop at the end that resizes and moves all windows after they have all been opened. This was not very performant with large numbers of windows. Instead it opens a window and then immediately resizes and moves that window and then continues to the next window.

--- manydoom.py
+++ manydoom.py
@@ -3,55 +3,61 @@ import time
 import pygetwindow as gw
 import pyautogui
 
-n = 200
+n = 1000
+x=0
+y=0
+xPosition=0
+yPosition=0
+windowTitle = "The Ultimate DOOM"
+windowWidth = 320
+windowHeight = 240
 
-def launchChocolateDoom(n: int):
+def resize_and_tile_window():
+    
+    global n
+    global x
+    global y
+    global xPosition
+    global yPosition
+    global windowTitle
+    global windowWidth
+    global windowHeight
+    xDistanceBetweenTiles = 40
+    yDistanceBetweenTiles = 120
+
+    #Get Screen dimensions
+    screen_width, screen_height = pyautogui.size()    
+    
+    window = gw.getActiveWindow()
+    try:
+        xPosition = xDistanceBetweenTiles * x
+        
+        if (xPosition > (screen_width - windowWidth)): #if we reach the end of the screen width start a new vertical row
+            x = 0
+            xPosition = 0
+            y += 1
+            
+            yPosition = yDistanceBetweenTiles * y
+            
+            if(yPosition > (screen_height - windowHeight)): #if we reach the end of the screen height start back from 0,0
+                yPosition=0
+                y=0                                  
+        
+        window.resizeTo(windowWidth,windowHeight)
+        window.moveTo(xPosition,yPosition)
+        
+        x += 1                     
+    except Exception as e:
+        print(f"Error managing window: {e}")
+
+def launchChocolateDoom():
+    global n
+
     for i in range(n):
         os.system(f'start C:\chocolate-doom-3.0.1-win32\chocolate-doom.exe -extraconfig C:\chocolate-doom-3.0.1-win32\chocolate-doom.cfg -1 -nosound -nosfx -nomusic -iwad C:\chocolate-doom-3.0.1-win32\doom.wad')
+        w = i+1
+        print(f"Opening DOOM Window {w}")
+        time.sleep(0.100)
+        resize_and_tile_window()
 
-def resize_and_tile_windows(title: str, width: int, height: int, n: int):
-    time.sleep(10) #wait for windows to open
-    windows = gw.getAllWindows()
-
-    # Screen dimensions
-    screen_width, screen_height = pyautogui.size()    
-
-    #calculate number of windows per row and column
-    xDistanceBetweenTiles = 175
-    yDistanceBetweenTiles = 200
-
-    x=0
-    y=0
-    w=0
-
-    xPosition=0
-    yPosition=0
-
-    # i counts the total number of windows (including VSCode & Task Manager) but we only want DOOM windows.
-    # So using w below inside if condition for Doom Window
-    # I'm not used to Python so I didn't have a graceful way to remove the unused i varible from the loop    
-    for i,window in enumerate(windows[:n+3]): # Added 3 to n value since I also expect 2 more open (non-Doom) windows while running
-        if window.title.startswith(title):
-            try:
-                xPosition = xDistanceBetweenTiles * x
-                
-                if (xPosition > (screen_width - width)): #if we reach the end of the screen width start a new vertical row
-                    x = 0
-                    xPosition = 0
-                    y += 1
-                    
-                    yPosition = yDistanceBetweenTiles * y
-                    
-                    if(yPosition > (screen_height - height)): #if we reach the end of the screen height start back from 0,0
-                        yPosition=0
-                        y=0                                  
-                
-                window.moveTo(xPosition,yPosition)
-                
-                x += 1
-                w += 1                 
-            except Exception as e:
-                print(f"Error managing window: {e}")
-
-launchChocolateDoom(n)
-resize_and_tile_windows("The Ultimate DOOM", 320, 240, n)
+launchChocolateDoom()
